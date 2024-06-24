1f7dce3de8b137fbb3582726129a909bd2fbfc0b
Omri Steinberg <omri.steinberg@abra-it.com>
2024-06-25 01:51:08 +0300

    fix: public host address

--- watchme/settings.py
+++ watchme/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-omristg.azurewebsites.net", "127.0.0.1"]
+ALLOWED_HOSTS = ["omristg-django.azurewebsites.net", "127.0.0.1"]
 
 
 # Application definition
