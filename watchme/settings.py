fb1ea74d7149bca6f333dd1e7f9a288e15e2ffab
Omri Steinberg <omri.steinberg@abra-it.com>
2024-06-24 21:15:34 +0300

    feat: set host

--- watchme/settings.py
+++ watchme/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-omristg.azurewebsites.net"]
 
 
 # Application definition
